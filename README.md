3ff7294e1265e261e159489742e80e505cadffc2
iosenberg <70673719+iosenberg@users.noreply.github.com>
2023-10-11 13:15:14 -0400

    Fix dependencies / big reload files

--- README.md
+++
@@ -1,2 +0,0 @@
-# todo
-A todo WebApp!
--- src/test/java/dev/iosenberg/todo/TodoApplicationTests.java
+++ src/test/java/dev/iosenberg/todo/TodoApplicationTests.java
@@ -3,6 +3,10 @@ package dev.iosenberg.todo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class TodoApplicationTests {
 
